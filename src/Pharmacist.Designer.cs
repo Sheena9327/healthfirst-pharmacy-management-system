487fbfa3bc5878e892833a94bef2f6669cae918a
Shivani Gupta <shivani.gupta9327@gmail.com>
2026-08-08 20:11:29 -0400

    Add files via upload

---
+++ src/Pharmacist.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace LoginRegistrationForm
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridViewPrescriptions = new System.Windows.Forms.DataGridView();
+            this.labelPrescriptions = new System.Windows.Forms.Label();
+            this.labelMedicationDetails = new System.Windows.Forms.Label();
+            this.textBoxMedicineName = new System.Windows.Forms.TextBox();
+            this.textBoxDosage = new System.Windows.Forms.TextBox();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStopDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerExpiryDate = new System.Windows.Forms.DateTimePicker();
+            this.buttonMarkIssued = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrescriptions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPrescriptions
+            // 
+            this.dataGridViewPrescriptions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPrescriptions.Location = new System.Drawing.Point(20, 60);
+            this.dataGridViewPrescriptions.Name = "dataGridViewPrescriptions";
+            this.dataGridViewPrescriptions.RowHeadersWidth = 51;
+            this.dataGridViewPrescriptions.Size = new System.Drawing.Size(750, 200);
+            this.dataGridViewPrescriptions.TabIndex = 0;
+            // 
+            // labelPrescriptions
+            // 
+            this.labelPrescriptions.AutoSize = true;
+            this.labelPrescriptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelPrescriptions.Location = new System.Drawing.Point(20, 20);
+            this.labelPrescriptions.Name = "labelPrescriptions";
+            this.labelPrescriptions.Size = new System.Drawing.Size(201, 25);
+            this.labelPrescriptions.TabIndex = 1;
+            this.labelPrescriptions.Text = "Prescription Overview";
+            // 
+            // labelMedicationDetails
+            // 
+            this.labelMedicationDetails.AutoSize = true;
+            this.labelMedicationDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelMedicationDetails.Location = new System.Drawing.Point(20, 280);
+            this.labelMedicationDetails.Name = "labelMedicationDetails";
+            this.labelMedicationDetails.Size = new System.Drawing.Size(177, 25);
+            this.labelMedicationDetails.TabIndex = 2;
+            this.labelMedicationDetails.Text = "Medication Details:";
+            // 
+            // textBoxMedicineName
+            // 
+            this.textBoxMedicineName.Location = new System.Drawing.Point(150, 320);
+            this.textBoxMedicineName.Name = "textBoxMedicineName";
+            this.textBoxMedicineName.Size = new System.Drawing.Size(200, 22);
+            this.textBoxMedicineName.TabIndex = 3;
+            // 
+            // textBoxDosage
+            // 
+            this.textBoxDosage.Location = new System.Drawing.Point(150, 360);
+            this.textBoxDosage.Name = "textBoxDosage";
+            this.textBoxDosage.Size = new System.Drawing.Size(200, 22);
+            this.textBoxDosage.TabIndex = 4;
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(150, 400);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerStartDate.TabIndex = 5;
+            // 
+            // dateTimePickerStopDate
+            // 
+            this.dateTimePickerStopDate.Location = new System.Drawing.Point(150, 440);
+            this.dateTimePickerStopDate.Name = "dateTimePickerStopDate";
+            this.dateTimePickerStopDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerStopDate.TabIndex = 6;
+            // 
+            // dateTimePickerExpiryDate
+            // 
+            this.dateTimePickerExpiryDate.Location = new System.Drawing.Point(150, 480);
+            this.dateTimePickerExpiryDate.Name = "dateTimePickerExpiryDate";
+            this.dateTimePickerExpiryDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerExpiryDate.TabIndex = 7;
+            // 
+            // buttonMarkIssued
+            // 
+            this.buttonMarkIssued.Location = new System.Drawing.Point(150, 520);
+            this.buttonMarkIssued.Name = "buttonMarkIssued";
+            this.buttonMarkIssued.Size = new System.Drawing.Size(200, 30);
+            this.buttonMarkIssued.TabIndex = 8;
+            this.buttonMarkIssued.Text = "Mark as Issued";
+            this.buttonMarkIssued.UseVisualStyleBackColor = true;
+            this.buttonMarkIssued.Click += new System.EventHandler(this.buttonMarkIssued_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(-4, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Medicine Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(-4, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dosage";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(-4, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Start";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(-4, 437);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Stop";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(-4, 477);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Expiry";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonMarkIssued);
+            this.Controls.Add(this.dateTimePickerExpiryDate);
+            this.Controls.Add(this.dateTimePickerStopDate);
+            this.Controls.Add(this.dateTimePickerStartDate);
+            this.Controls.Add(this.textBoxDosage);
+            this.Controls.Add(this.textBoxMedicineName);
+            this.Controls.Add(this.labelMedicationDetails);
+            this.Controls.Add(this.labelPrescriptions);
+            this.Controls.Add(this.dataGridViewPrescriptions);
+            this.Name = "MainForm";
+            this.Text = "Pharmacist Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrescriptions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridViewPrescriptions;
+        private System.Windows.Forms.Label labelPrescriptions;
+        private System.Windows.Forms.Label labelMedicationDetails;
+        private System.Windows.Forms.TextBox textBoxMedicineName;
+        private System.Windows.Forms.TextBox textBoxDosage;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStopDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerExpiryDate;
+        private System.Windows.Forms.Button buttonMarkIssued;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ src/SalesM.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LoginRegistrationForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadMedicineData();
+        }
+
+        // Simulate loading medicine data
+        private void LoadMedicineData()
+        {
+            DataTable medicineTable = new DataTable();
+            medicineTable.Columns.Add("Medicine Name", typeof(string));
+            medicineTable.Columns.Add("Quantity", typeof(int));
+            medicineTable.Columns.Add("Price", typeof(decimal));
+            medicineTable.Columns.Add("Expiry", typeof(DateTime));
+
+            // Sample medicine data
+            medicineTable.Rows.Add("Paracetamol", 100, 10.5m, DateTime.Now.AddMonths(6));
+            medicineTable.Rows.Add("Ibuprofen", 200, 15.0m, DateTime.Now.AddMonths(-1)); // Expired
+            medicineTable.Rows.Add("Amoxicillin", 50, 12.5m, DateTime.Now.AddMonths(3));
+
+            // Set the data source
+            dataGridViewMedicines.DataSource = medicineTable;
+
+            // Mark expired medicines in red
+            MarkExpiredMedicines(medicineTable);
+        }
+
+        // Mark expired medicines in red
+        private void MarkExpiredMedicines(DataTable medicineTable)
+        {
+            foreach (DataGridViewRow row in dataGridViewMedicines.Rows)
+            {
+                DateTime expiryDate = Convert.ToDateTime(row.Cells["Expiry"].Value);
+                if (expiryDate < DateTime.Now)
+                {
+                    row.DefaultCellStyle.BackColor = Color.Red; // Mark as expired
+                }
+            }
+        }
+
+        // Add new medicine to the table
+        private void buttonAddMedicine_Click(object sender, EventArgs e)
+        {
+            string medicineName = textBoxMedicineName.Text;
+            int quantity = (int)numericUpDownQuantity.Value;
+            decimal price = numericUpDownPrice.Value;
+            DateTime expiry = dateTimePickerExpiry.Value;
+
+            if (string.IsNullOrEmpty(medicineName))
+            {
+                MessageBox.Show("Please enter a valid medicine name.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Get the current data from the DataGridView
+            var medicineTable = (DataTable)dataGridViewMedicines.DataSource;
+
+            // Add the new medicine
+            medicineTable.Rows.Add(medicineName, quantity, price, expiry);
+
+            // Mark expired medicines in red
+            MarkExpiredMedicines(medicineTable);
+
+            MessageBox.Show("Medicine added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Update selected medicine
+        private void buttonUpdateMedicine_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMedicines.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridViewMedicines.SelectedRows[0];
+
+                string medicineName = textBoxMedicineName.Text;
+                int quantity = (int)numericUpDownQuantity.Value;
+                decimal price = numericUpDownPrice.Value;
+                DateTime expiry = dateTimePickerExpiry.Value;
+
+                if (string.IsNullOrEmpty(medicineName))
+                {
+                    MessageBox.Show("Please enter a valid medicine name.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Update the selected row's details
+                selectedRow.Cells["Medicine Name"].Value = medicineName;
+                selectedRow.Cells["Quantity"].Value = quantity;
+                selectedRow.Cells["Price"].Value = price;
+                selectedRow.Cells["Expiry"].Value = expiry;
+
+                // Recheck and mark expired medicines
+                var medicineTable = (DataTable)dataGridViewMedicines.DataSource;
+                MarkExpiredMedicines(medicineTable);
+
+                MessageBox.Show("Medicine details updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a medicine to update.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Delete selected medicine
+        private void buttonDeleteMedicine_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMedicines.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridViewMedicines.SelectedRows[0];
+                dataGridViewMedicines.Rows.Remove(selectedRow);
+
+                MessageBox.Show("Medicine deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a medicine to delete.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
